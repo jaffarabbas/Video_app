641df3fb7d1cd15399fdf2eb454ecf870a6cc18e
jaffarabbas <gamaportal8@gmail.com>
2020-10-28 18:38:37 +0500

    10-28-2020

---
+++ JaffPlayer/app/src/main/java/com/example/jaffplayer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jaffplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView video_view = findViewById(R.id.video);
+        video_view.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.clip1);
+        MediaController controls = new MediaController(this);
+        video_view.setMediaController(controls);
+        controls.setAnchorView(video_view);
+        video_view.start();
+    }
+}
